8e19b45a283a9789aa8cd0429c7a602c7948977d
Bogdan Kovtun <psirex48@gmail.com>
2021-07-13 01:47:15 +0300

    Update comments and tests for TopUpLegoProgram

--- contracts/EVMScriptFactories/TopUpLegoProgram.sol
+++ contracts/EVMScriptFactories/TopUpLegoProgram.sol
@@ -9,9 +9,28 @@ import "../interfaces/IFinance.sol";
 import "../libraries/EVMScriptCreator.sol";
 import "../interfaces/IEVMScriptFactory.sol";
 
+/// @notice Creates EVMScript to top up the address of the LEGO program
 contract TopUpLegoProgram is TrustedCaller, IEVMScriptFactory {
+    // -------------
+    // ERRORS
+    // -------------
+    string private constant ERROR_LENGTH_MISMATCH = "LENGTH_MISMATCH";
+    string private constant ERROR_EMPTY_DATA = "EMPTY_DATA";
+    string private constant ERROR_ZERO_AMOUNT = "ZERO_AMOUNT";
+
+    // -------------
+    // VARIABLES
+    // -------------
+
+    /// @notice Address of Aragon's Finance contract
     IFinance public immutable finance;
+
+    /// @notice Address of LEGO program
     address public immutable legoProgram;
+
+    // -------------
+    // CONSTRUCTOR
+    // -------------
 
     constructor(
         address _trustedCaller,
@@ -22,6 +41,15 @@ contract TopUpLegoProgram is TrustedCaller, IEVMScriptFactory {
         legoProgram = _legoProgram;
     }
 
+    // -------------
+    // EXTERNAL METHODS
+    // -------------
+
+    /// @notice Creates EVMScript to top up the address of the LEGO program
+    /// @param _creator Address who creates EVMScript
+    /// @param _evmScriptCallData Encoded tuple: (address[] _rewardTokens, uint256[] _amounts) where
+    /// _rewardTokens - addresses of ERC20 tokens (zero address for ETH) to transfer
+    /// _amounts - corresponding amount of tokens to transfer
     function createEVMScript(address _creator, bytes memory _evmScriptCallData)
         external
         view
@@ -31,7 +59,7 @@ contract TopUpLegoProgram is TrustedCaller, IEVMScriptFactory {
     {
         (address[] memory rewardTokens, uint256[] memory amounts) =
             _decodeEVMScriptCallData(_evmScriptCallData);
-        _validateMotionData(rewardTokens, amounts);
+        _validateEVMScriptCallDAta(rewardTokens, amounts);
 
         bytes[] memory paymentsCallData = new bytes[](rewardTokens.length);
         for (uint256 i = 0; i < rewardTokens.length; ++i) {
@@ -51,29 +79,39 @@ contract TopUpLegoProgram is TrustedCaller, IEVMScriptFactory {
             );
     }
 
-    function _validateMotionData(address[] memory _rewardTokens, uint256[] memory _amounts)
-        private
-        pure
-    {
-        require(_rewardTokens.length == _amounts.length, "LENGTH_MISMATCH");
-        require(_rewardTokens.length > 0, "EMPTY_DATA");
-        for (uint256 i = 0; i < _rewardTokens.length; ++i) {
-            require(_amounts[i] > 0, "ZERO_AMOUNT");
-        }
-    }
-
+    /// @notice Decodes call data used by createEVMScript method
+    /// @param _evmScriptCallData Encoded tuple: (address[] _rewardTokens, uint256[] _amounts) where
+    /// _rewardTokens - addresses of ERC20 tokens (zero address for ETH) to transfer
+    /// _amounts - corresponding amount of tokens to transfer
+    /// @return _rewardTokens Addresses of ERC20 tokens (zero address for ETH) to transfer
+    /// @return _amounts Amounts of tokens to transfer
     function decodeEVMScriptCallData(bytes memory _evmScriptCallData)
         external
         pure
-        returns (address[] memory rewardTokens, uint256[] memory amounts)
+        returns (address[] memory _rewardTokens, uint256[] memory _amounts)
     {
         return _decodeEVMScriptCallData(_evmScriptCallData);
     }
 
-    function _decodeEVMScriptCallData(bytes memory _evmScriptCallData)
-        internal
+    // ------------------
+    // PRIVATE METHODS
+    // ------------------
+
+    function _validateEVMScriptCallDAta(address[] memory _rewardTokens, uint256[] memory _amounts)
+        private
         pure
-        returns (address[] memory rewardTokens, uint256[] memory amounts)
+    {
+        require(_rewardTokens.length == _amounts.length, ERROR_LENGTH_MISMATCH);
+        require(_rewardTokens.length > 0, ERROR_EMPTY_DATA);
+        for (uint256 i = 0; i < _rewardTokens.length; ++i) {
+            require(_amounts[i] > 0, ERROR_ZERO_AMOUNT);
+        }
+    }
+
+    function _decodeEVMScriptCallData(bytes memory _evmScriptCallData)
+        private
+        pure
+        returns (address[] memory _rewardTokens, uint256[] memory _amounts)
     {
         return abi.decode(_evmScriptCallData, (address[], uint256[]));
     }
--- tests/evm_script_factories/test_top_up_lego_program.py
+++ tests/evm_script_factories/test_top_up_lego_program.py
@@ -11,18 +11,15 @@ REWARD_AMOUNTS = [10 ** 18, 2 * 10 ** 18]
 
 
 def test_deploy(owner, finance, lego_program):
-    "Must deploy contract with correct params"
-
+    "Must deploy contract with correct data"
     contract = owner.deploy(TopUpLegoProgram, owner, finance, lego_program)
-
     assert contract.trustedCaller() == owner
     assert contract.finance() == finance
     assert contract.legoProgram() == lego_program
 
 
 def test_create_evm_script_called_by_stranger(stranger, top_up_lego_program):
-    "Must fail with error 'CALLER_IS_FORBIDDEN'"
-
+    "Must fail with error 'CALLER_IS_FORBIDDEN'  when creator isn't trustedCaller"
     with reverts("CALLER_IS_FORBIDDEN"):
         top_up_lego_program.createEVMScript(
             stranger, encode_call_data([], []), {"from": stranger}
@@ -30,8 +27,7 @@ def test_create_evm_script_called_by_stranger(stranger, top_up_lego_program):
 
 
 def test_create_evm_script_data_length_mismatch(owner, top_up_lego_program):
-    "Must fail with error 'LENGTH_MISMATCH'"
-
+    "Must fail with error 'LENGTH_MISMATCH' when rewardTokens and amounts has different lengths"
     with reverts("LENGTH_MISMATCH"):
         top_up_lego_program.createEVMScript(
             owner,
@@ -40,14 +36,13 @@ def test_create_evm_script_data_length_mismatch(owner, top_up_lego_program):
 
 
 def test_create_evm_script_empty_data(owner, top_up_lego_program):
-    "Must fail with error 'EMPTY_DATA'"
-
+    "Must fail with error 'EMPTY_DATA' when called with empty lists"
     with reverts("EMPTY_DATA"):
         top_up_lego_program.createEVMScript(owner, encode_call_data([], []))
 
 
 def test_create_evm_script_zero_amount(owner, top_up_lego_program):
-    "Must fail with error 'ZERO_AMOUNT'"
+    "Must fail with error 'ZERO_AMOUNT' if some value in amounts has zero value"
     amounts = [1 ** 18, 0]
 
     with reverts("ZERO_AMOUNT"):
@@ -59,8 +54,7 @@ def test_create_evm_script_zero_amount(owner, top_up_lego_program):
 def test_create_evm_script(
     owner, lego_program, top_up_lego_program, finance, ldo_token
 ):
-    "Must create correct evm script"
-
+    "Must create correct EVMScript if all requirements are met"
     evm_script = top_up_lego_program.createEVMScript(
         owner, encode_call_data(REWARD_TOKENS, REWARD_AMOUNTS)
     )
@@ -92,6 +86,7 @@ def test_create_evm_script(
 
 
 def test_decode_evm_script_call_data(top_up_reward_programs):
+    "Must decode EVMScript call data correctly"
     assert top_up_reward_programs.decodeEVMScriptCallData(
         encode_call_data(REWARD_TOKENS, REWARD_AMOUNTS)
     ) == (REWARD_TOKENS, REWARD_AMOUNTS)
