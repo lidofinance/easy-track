ee5835d955ff565b642f47cbbba636bd7f9139f1
Bogdan Kovtun <psirex48@gmail.com>
2021-09-22 17:19:20 +0300

    Add additional test cases

--- tests/test_evm_script_factories_registry.py
+++ tests/test_evm_script_factories_registry.py
@@ -1,23 +1,27 @@
+import pytest
 import random
-from brownie import accounts, reverts, EVMScriptFactoriesRegistry
-from utils.test_helpers import access_controll_revert_message
+from brownie import reverts
 
 
-def test_deploy(owner):
+@pytest.fixture(scope="module")
+def extra_evm_script_factories(accounts):
+    return accounts[3:8]
+
+
+def test_deploy(owner, EVMScriptFactoriesRegistry):
     "Must deploy MotionsRegistry contract with correct params"
     contract = owner.deploy(EVMScriptFactoriesRegistry, owner)
-
-    # roles
     assert contract.hasRole(contract.DEFAULT_ADMIN_ROLE(), owner)
 
 
 def test_add_evm_script_factory_called_without_permissions(
-    stranger, evm_script_factories_registry
+    stranger, evm_script_factories_registry, access_controll_revert_message
 ):
     "Must revert with correct Access Control message if called by address without 'DEFAULT_ADMIN_ROLE'"
+    permissions = stranger.address + "ffccddee"
     with reverts(access_controll_revert_message(stranger)):
         evm_script_factories_registry.addEVMScriptFactory(
-            stranger, b"", {"from": stranger}
+            stranger, permissions, {"from": stranger}
         )
 
 
@@ -27,7 +31,7 @@ def test_add_evm_script_factory_empty_permissions(
     "Must revert with message 'INVALID_PERMISSIONS' if called with empty permissions"
     with reverts("INVALID_PERMISSIONS"):
         evm_script_factories_registry.addEVMScriptFactory(
-            stranger, b"", {"from": owner}
+            stranger, "0x", {"from": owner}
         )
 
 
@@ -72,21 +76,32 @@ def test_remove_evm_script_factory_not_found(
         evm_script_factories_registry.removeEVMScriptFactory(stranger, {"from": owner})
 
 
-def test_remove_evm_script_factory(owner, stranger, evm_script_factories_registry):
+def test_remove_evm_script_factory(
+    owner, stranger, evm_script_factories_registry, extra_evm_script_factories
+):
     "Must remove EVMScript factory from the list of allowed EVMScript factories"
     "and emit EVMScriptFactoryRemoved(_evmScriptFactory) event"
     # add many evm script factories
-    evm_script_factories = accounts[3:8]
     permissions = stranger.address + "ffccddee"
-    for evm_script_factory in evm_script_factories:
+    for evm_script_factory in extra_evm_script_factories:
         evm_script_factories_registry.addEVMScriptFactory(
             evm_script_factory, permissions, {"from": owner}
         )
 
-    # remove evm scripts in random order
-    # and check that was deleted correct script
-    while len(evm_script_factories) > 0:
-        index = random.randint(0, len(evm_script_factories) - 1)
+    # make a copy to avoid modifying the fixture object when popping
+    evm_script_factories = extra_evm_script_factories.copy()
+
+    # sets the order in which evm script factories will be removed
+    # 1. remove factory at index=3: [0, 1, 2, (3), 4] -> [0, 1, 2, 4]
+    # 2. remove factory at index=1: [0, (1), 2, 4] -> [0, 4, 2]
+    # 3. remove factory at index=0: [(0), 4, 2] -> [2, 4]
+    # 4. remove factory at index=1: [2, (4)]-> [2]
+    # 5. remove factory at index=0: [(2)] -> []
+    removing_order = [3, 1, 0, 1, 0]
+
+    # remove evm scripts in predefined order and check
+    # that was deleted correct evm script factory
+    for index in removing_order:
         evm_script_factory_to_remove = evm_script_factories.pop(index)
 
         assert evm_script_factories_registry.isEVMScriptFactory(
