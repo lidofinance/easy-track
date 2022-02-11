7e9ca54902fc9a4ae266bc895780e85b82282f69
Eugene Mamin <TheDZhon@gmail.com>
2022-02-11 17:24:03 +0000

    fix: improve logging funcs
    Let there be more colors.

--- utils/log.py
+++ utils/log.py
@@ -1,9 +1,9 @@
 color_hl = "\x1b[38;5;141m"
 color_green = "\033[92m"
 color_yellow = "\033[93m"
+color_magenta = "\033[0;35m"
 color_gray = "\x1b[0;m"
 color_end = "\033[0m"
-
 
 def highlight(text, color=color_hl):
     return f"{color}{text}{color_end}"
@@ -17,8 +17,10 @@ def ok(text, value=None):
 
     print(result)
 
+def br():
+    print(highlight("-" * 10, color_gray))
 
-def nb(text, value=None):
+def nb(text, value=None, color_hl=color_hl):
     result = highlight(">>>> ", color_yellow) + text
 
     if value is not None:
