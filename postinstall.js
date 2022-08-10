5f159666b983818441806771c4131d59c673b9f9
Eugene Mamin <TheDZhon@gmail.com>
2022-08-10 20:02:57 +0000

    fix: update deps mgmt
    Use yarn, add solidity-bytes-utils

--- postinstall.js
+++ postinstall.js
@@ -1,17 +1,14 @@
 const path = require("path");
 const copy = require("recursive-copy");
 
-async function renameOpenzeppelinDependencies() {
+async function renameDependencies(remapping) {
   const nodeModulesPath = path.join(__dirname, "node_modules");
-  const remapping = {
-    "@openzeppelin/contracts": "OpenZeppelin/openzeppelin-contracts@4.3.2/contracts",
-  };
 
   for (const localPath of Object.keys(remapping)) {
     const from = path.join(nodeModulesPath, localPath);
     const to = path.join(nodeModulesPath, remapping[localPath]);
     try {
-      console.log(`Move ${localPath} to ${remapping[localPath]}`);
+      console.log(`Copy ${localPath} to ${remapping[localPath]}`);
       const res = await copy(from, to, {
         overwrite: true,
         expand: true,
@@ -25,4 +22,7 @@ async function renameOpenzeppelinDependencies() {
   }
 }
 
-renameOpenzeppelinDependencies();
+renameDependencies({
+  "@openzeppelin/contracts": "OpenZeppelin/openzeppelin-contracts@4.3.2/contracts",
+  "solidity-bytes-utils/contracts": "GNSPS/solidity-bytes-utils@0.8.0/contracts"
+});
