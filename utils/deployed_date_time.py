0be1b337b2addfde9c8ff5f718b96c239c391c66
rkolpakov <rkolpakov94@gmail.com>
2022-11-08 17:53:08 +0400

    feat: add goerli datetime lib

--- utils/deployed_date_time.py
+++ utils/deployed_date_time.py
@@ -2,7 +2,5 @@ def date_time_contract(network: str = "mainnet") -> str:
     if network == "mainnet":
         return "0x23d23d8F243e57d0b924bff3A3191078Af325101"
     if network == "goerli":
-        return ""
-    raise NameError(
-        f"""Unknown network "{network}". Supported networks: mainnet, goerli."""
-    )
+        return "0xb1e4de1092D0D32613e4BbFBf4D68650862f43A6"
+    raise NameError(f"""Unknown network "{network}". Supported networks: mainnet, goerli.""")
