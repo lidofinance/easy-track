ad790d429f0dde034244c31c35a911217878f7ef
Aleksandr Tarelkin <tarelkinaa@bk.ru>
2023-09-12 22:25:16 +0300

    aragon acl 2 roles w diff params

--- tests/test_aragon_acl.py
+++ tests/test_aragon_acl.py
@@ -59,11 +59,77 @@ def test_aragon_acl_role_with_permission(acl, voting, stranger):
     )
 
     assert acl.hasPermission(stranger, voting, TEST_ROLE) == False
+    # assert False == True
+    # assert (
+    #     acl.hasPermission["address,address,bytes32,uint[]"](
+    #         stranger, voting, TEST_ROLE, [new_permission_param]
+    #     )
+    #     == True
+    # )
+    assert acl.getPermissionParamsLength(stranger, voting, TEST_ROLE) == 1
+    assert acl.getPermissionParam(stranger, voting, TEST_ROLE, 0) == new_permission
+
+def test_aragon_acl_two_roles_with_different_params(acl, voting, stranger):
+
+    # Grant role with params (0, 1, 3)
+    permission = (0, 1, 3)
+    permission_param = convert.to_uint(
+        (permission[0] << 248) + (permission[1] << 240) + permission[2], "uint256"
+    )
+
+    assert acl.hasPermission(stranger, voting, TEST_ROLE) == False
+
+    acl.createPermission(voting, voting, TEST_ROLE, voting, {"from": voting})
+    acl.grantPermissionP(
+        stranger, voting, TEST_ROLE, [permission_param], {"from": voting}
+    )
+
+
+    # Test role (0, 1, 3)
+    assert acl.hasPermission(stranger, voting, TEST_ROLE) == False
     assert (
         acl.hasPermission["address,address,bytes32,uint[]"](
-            stranger, voting, TEST_ROLE, [new_permission_param]
+            stranger, voting, TEST_ROLE, [permission_param]
         )
         == True
     )
     assert acl.getPermissionParamsLength(stranger, voting, TEST_ROLE) == 1
+    assert acl.getPermissionParam(stranger, voting, TEST_ROLE, 0) == permission
+
+
+    # Grant role with params (1, 2, 4)
+    new_permission = (1, 2, 4)
+    new_permission_param = convert.to_uint(
+        (new_permission[0] << 248) + (new_permission[1] << 240) + new_permission[2],
+        "uint256",
+    )
+
+    acl.grantPermissionP(
+        stranger, voting, TEST_ROLE, [new_permission_param], {"from": voting}
+    )
+
+    # Test role (1,2,4)
+    assert acl.hasPermission(stranger, voting, TEST_ROLE) == False
+    # assert False == True
+    # assert (
+    #     acl.hasPermission["address,address,bytes32,uint[]"](
+    #         stranger, voting, TEST_ROLE, [new_permission_param]
+    #     )
+    #     == True
+    # )
+    assert acl.getPermissionParamsLength(stranger, voting, TEST_ROLE) == 1
     assert acl.getPermissionParam(stranger, voting, TEST_ROLE, 0) == new_permission
+
+
+    # Test role (0, 1, 3)
+    assert acl.hasPermission(stranger, voting, TEST_ROLE) == False
+    # assert False == True
+    # assert (
+    #     acl.hasPermission["address,address,bytes32,uint[]"](
+    #         stranger, voting, TEST_ROLE, [permission_param]
+    #     )
+    #     == True
+    # )
+    assert acl.getPermissionParamsLength(stranger, voting, TEST_ROLE) == 1
+    # assert (1, 2, 4) == (0, 1, 3)
+    # assert acl.getPermissionParam(stranger, voting, TEST_ROLE, 0) == permission
