131c77af525f2a9f3e9e63f9906c900ffdbcfceb
vgorkavenko <vladimir.g@lido.fi>
2024-06-24 15:43:44 +0400

    feat: holesky deployment

--- utils/csm.py
+++ utils/csm.py
@@ -8,13 +8,15 @@ def addresses(network=DEFAULT_NETWORK):
     if network == "mainnet" or network == "mainnet-fork":
         raise NotImplementedError("Mainnet addresses do not exist yet")
     if network == "holesky" or network == "holesky-fork":
-        raise NotImplementedError("Holesky addresses do not exist yet")
+        return CSMAddressesSetup(
+            module="0x4562c3e63c2e586cD1651B958C22F88135aCAd4f",
+        )
     if network == "holesky-devnet" or network == "holesky-devnet-fork":
         return CSMAddressesSetup(
             module="0x26aBc20a47f7e8991F1d26Bf0fC2bE8f24E9eF2A",
         )
     raise NameError(
-        f"Unknown network '{network}'. Supported networks: mainnet, mainnet-fork, holesky, holesky-fork, holesky-devnet0, holesky-devnet0-fork"
+        f"Unknown network '{network}'. Supported networks: mainnet, mainnet-fork, holesky, holesky-fork, holesky-devnet, holesky-devnet-fork"
     )
 
 
