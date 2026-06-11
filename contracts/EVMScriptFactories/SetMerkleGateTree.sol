a6db78fcf86eaa123e2a682274134cb98be7ad90
madlabman <10616301+madlabman@users.noreply.github.com>
2026-06-11 15:19:18 +0000

    chore: commit tree state to the motion calldata

--- contracts/EVMScriptFactories/SetMerkleGateTree.sol
+++ contracts/EVMScriptFactories/SetMerkleGateTree.sol
@@ -8,23 +8,27 @@ import "../libraries/EVMScriptCreator.sol";
 import "../interfaces/IEVMScriptFactory.sol";
 import "../interfaces/IMerkleGate.sol";
 
+interface ISetMerkleGateTree is IEVMScriptFactory {
+    function validateInputData(
+        address gate,
+        bytes32 currentTreeRoot,
+        string memory currentTreeCid,
+        bytes32 newTreeRoot,
+        string memory newTreeCid
+    ) external view;
+}
+
 /// @author vgorkavenko
 /// @notice Creates EVMScript to set tree for Module's Gate that implements IMerkleGate
-contract SetMerkleGateTree is TrustedCaller, IEVMScriptFactory {
-
+contract SetMerkleGateTree is TrustedCaller, ISetMerkleGateTree {
     // -------------
     // ERRORS
     // -------------
-    string private constant ERROR_EMPTY_TREE_ROOT =
-        "EMPTY_TREE_ROOT";
-    string private constant ERROR_EMPTY_TREE_CID =
-        "EMPTY_TREE_CID";
-    string private constant ERROR_SAME_TREE_CID =
-        "SAME_TREE_CID";
-    string private constant ERROR_SAME_TREE_ROOT =
-        "SAME_TREE_ROOT";
-    string private constant ERROR_CURRENT_VALUES_MISMATCH =
-        "CURRENT_VALUES_MISMATCH";
+    string private constant ERROR_EMPTY_TREE_ROOT = "EMPTY_TREE_ROOT";
+    string private constant ERROR_EMPTY_TREE_CID = "EMPTY_TREE_CID";
+    string private constant ERROR_SAME_TREE_CID = "SAME_TREE_CID";
+    string private constant ERROR_SAME_TREE_ROOT = "SAME_TREE_ROOT";
+    string private constant ERROR_CURRENT_VALUES_MISMATCH = "CURRENT_VALUES_MISMATCH";
 
     // -------------
     // VARIABLES
@@ -37,9 +41,7 @@ contract SetMerkleGateTree is TrustedCaller, IEVMScriptFactory {
     // CONSTRUCTOR
     // -------------
 
-    constructor(address _trustedCaller, string memory _name)
-        TrustedCaller(_trustedCaller)
-    {
+    constructor(address _trustedCaller, string memory _name) TrustedCaller(_trustedCaller) {
         name = _name;
     }
 
@@ -72,13 +74,21 @@ contract SetMerkleGateTree is TrustedCaller, IEVMScriptFactory {
             string memory newTreeCid
         ) = _decodeEVMScriptCallData(_evmScriptCallData);
 
-        _validateInputData(gate, currentTreeRoot, currentTreeCid, newTreeRoot, newTreeCid);
+        validateInputData(gate, currentTreeRoot, currentTreeCid, newTreeRoot, newTreeCid);
 
-        return EVMScriptCreator.createEVMScript(
-            gate,
-            IMerkleGate.setTreeParams.selector,
-            abi.encode(newTreeRoot, newTreeCid)
-        );
+        address[] memory toAddresses = new address[](2);
+        bytes4[] memory methodIds = new bytes4[](2);
+        bytes[] memory data = new bytes[](2);
+
+        toAddresses[0] = address(this);
+        methodIds[0] = ISetMerkleGateTree.validateInputData.selector;
+        data[0] = abi.encode(gate, currentTreeRoot, currentTreeCid, newTreeRoot, newTreeCid);
+
+        toAddresses[1] = gate;
+        methodIds[1] = IMerkleGate.setTreeParams.selector;
+        data[1] = abi.encode(newTreeRoot, newTreeCid);
+
+        return EVMScriptCreator.createEVMScript(toAddresses, methodIds, data);
     }
 
     /// @notice Decodes call data used by createEVMScript method
@@ -92,9 +102,37 @@ contract SetMerkleGateTree is TrustedCaller, IEVMScriptFactory {
     function decodeEVMScriptCallData(bytes calldata _evmScriptCallData)
         external
         pure
-        returns (address, bytes32, string memory, bytes32, string memory)
+        returns (
+            address,
+            bytes32,
+            string memory,
+            bytes32,
+            string memory
+        )
     {
         return _decodeEVMScriptCallData(_evmScriptCallData);
+    }
+
+    function validateInputData(
+        address gate,
+        bytes32 currentTreeRoot,
+        string memory currentTreeCid,
+        bytes32 newTreeRoot,
+        string memory newTreeCid
+    ) public view override {
+        IMerkleGate merkleGate = IMerkleGate(gate);
+        bytes32 onChainTreeRoot = merkleGate.treeRoot();
+        bytes32 onChainTreeCidHash = keccak256(bytes(merkleGate.treeCid()));
+
+        require(currentTreeRoot == onChainTreeRoot, ERROR_CURRENT_VALUES_MISMATCH);
+        require(
+            keccak256(bytes(currentTreeCid)) == onChainTreeCidHash,
+            ERROR_CURRENT_VALUES_MISMATCH
+        );
+        require(newTreeRoot != bytes32(0), ERROR_EMPTY_TREE_ROOT);
+        require(bytes(newTreeCid).length > 0, ERROR_EMPTY_TREE_CID);
+        require(newTreeRoot != onChainTreeRoot, ERROR_SAME_TREE_ROOT);
+        require(keccak256(bytes(newTreeCid)) != onChainTreeCidHash, ERROR_SAME_TREE_CID);
     }
 
     // ------------------
@@ -104,27 +142,14 @@ contract SetMerkleGateTree is TrustedCaller, IEVMScriptFactory {
     function _decodeEVMScriptCallData(bytes calldata _evmScriptCallData)
         private
         pure
-        returns (address, bytes32, string memory, bytes32, string memory)
+        returns (
+            address,
+            bytes32,
+            string memory,
+            bytes32,
+            string memory
+        )
     {
         return abi.decode(_evmScriptCallData, (address, bytes32, string, bytes32, string));
-    }
-
-    function _validateInputData(
-        address gate,
-        bytes32 currentTreeRoot,
-        string memory currentTreeCid,
-        bytes32 newTreeRoot,
-        string memory newTreeCid
-    ) private view {
-        IMerkleGate merkleGate = IMerkleGate(gate);
-        bytes32 onChainTreeRoot = merkleGate.treeRoot();
-        bytes32 onChainTreeCidHash = keccak256(bytes(merkleGate.treeCid()));
-
-        require(currentTreeRoot == onChainTreeRoot, ERROR_CURRENT_VALUES_MISMATCH);
-        require(keccak256(bytes(currentTreeCid)) == onChainTreeCidHash, ERROR_CURRENT_VALUES_MISMATCH);
-        require(newTreeRoot != bytes32(0), ERROR_EMPTY_TREE_ROOT);
-        require(bytes(newTreeCid).length > 0, ERROR_EMPTY_TREE_CID);
-        require(newTreeRoot != onChainTreeRoot, ERROR_SAME_TREE_ROOT);
-        require(keccak256(bytes(newTreeCid)) != onChainTreeCidHash, ERROR_SAME_TREE_CID);
     }
 }
